edbefa2ec3a7f0115728e50d50c792f0c3da6ae5
Cruelkid <sonofammit@gmail.com>
2021-08-19 09:22:19 +0300

    moved authentication logic to auth context

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogin: (email, password) => {},
+    onLogout: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const loggedInInfo = localStorage.getItem("isUserLoggedIn");
+
+        if (loggedInInfo === "true") {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const loginHandler = () => {
+        localStorage.setItem("isUserLoggedIn", "true");
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.setItem("isUserLoggedIn", "false");
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogin: loginHandler,
+                onLogout: logoutHandler,
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    );
+};
 
 export default AuthContext;
